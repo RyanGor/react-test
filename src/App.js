f6e8f6667e5b3f17f38c3a3cf273e92739e05300
kavitasookrah <kavita@taplytics.com>
2018-10-02 14:07:21 -0400

    index.html

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <script type="text/javascript" src="https://js.taplytics.com/jssdk/54e9bd0339984587bf715fdc9c934e66.min.js"></script>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Test <code>src/App.js</code> and save to reload.
+            Integration <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
