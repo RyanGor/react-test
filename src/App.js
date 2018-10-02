c056e45a596f29221ab22acf07cf2d7693f83b79
kavitasookrah <kavita@taplytics.com>
2018-10-02 13:27:22 -0400

    something

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render()
+  
+  {
     return (
       <div className="App">
         <header className="App-header">
