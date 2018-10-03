ff0a8e0c92e806eb44a237190ac7ca2e17d4b0ce
kavitasookrah <kavita@taplytics.com>
2018-10-03 12:30:15 -0400

    button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-button-component';
+
 
 class App extends Component {
   render()
@@ -20,6 +22,10 @@ class App extends Component {
           >
             Learn React
           </a>
+
+          <Button onClick={() => alert('Welcome!')}>
+            Hop in!
+          </Button>
         </header>
       </div>
     );
