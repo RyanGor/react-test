d1304eb03f9aef4bef02a9c91c8927c8935ff277
kavitasookrah <kavita@taplytics.com>
2018-10-09 14:36:18 -0400

    test variable set up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,41 @@ import Button from 'react-button-component';
 
 
 class App extends Component {
+
+  constructor(props) {
+    this.state = {
+      buttonVar: false
+    }
+  }
+
+  componentDidMount() {
+    console.log("0", "MOUNTED")
+    Taplytics.variable("buttonVar", true, (buttonVar) => {
+      console.log("1", this.state.buttonVar, buttonVar)
+      if (buttonVar === true) {
+        this.setState({
+          buttonVar: true
+        })
+      }
+    });
+
+  }
   render()
+  let renderedButton
+  console.log("2", this.state.buttonVar)
+  if (this.state.buttonVar === true) {
+    renderedButton = (
+      <Button onClick={() => alert('Welcome!')}>
+      Hop in!
+    </Button>
+  )
+  } else {
+    renderedButton = (
+      <Button onClick={() => alert('Not Welcome!')}>
+        Hop in!
+      </Button>
+    )
+  }
   {
     return (
       <div className="App">
